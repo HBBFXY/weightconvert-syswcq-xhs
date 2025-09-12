1ca56bb36e346158785231a1f0e9bdb250261aad
syswcq-xhs <481124168@qq.com>
2025-09-12 11:14:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-weight_input=input()
-kg_to_lb=2.2046
-if    weight_input.endswith("kg"):
-     kg = float(weight_input[:-2])
-     lb = kg * kg_to_lb
-     print(f"对应的英制重量为{lb:.3f}磅")
-elif    weight_input.endswith("lb"):
-     lb = float(weight_input[:-2])
-     kg = lb / kg_to_lb
-     print(f"对应的公制重量为{kg:.3f}公斤")
+weight_input = input().strip()
+kg_to_pd = 2.2046
+if weight_input.endswith("kg"):
+    kg = float(weight_input[:-2])
+    pd = kg * kg_to_pd
+    print(f"对应的英制重量为{pd:.3f}磅")
+elif weight_input.endswith("pd"):
+    pd = float(weight_input[:-2])
+    kg = pd / kg_to_pd
+    print(f"对应的公制重量为{kg:.3f}公斤")
